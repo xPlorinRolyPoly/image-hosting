9c728acc5acbedbf7b6799f9dc537d04d9d5a1be
xPlorinRolyPoly <alpanachaphalkar@gmail.com>
2021-01-10 20:13:47 +0100

    image resizing using sharp functionality added.

--- application/app.js
+++ application/app.js
@@ -1,5 +1,6 @@
 const multipart = require("parse-multipart");
 const AWS = require("aws-sdk");
+const sharp = require('sharp');
 const s3 = new AWS.S3();
 
 const bucket = process.env.BUCKET_NAME
@@ -28,13 +29,31 @@ exports.lambdaHandler = function(event,context,callback){
         console.log(`File ${uploadFilename} uploaded successfully to Bucket ${bucket}.`);
     });
 
-    const signedS3Url = s3.getSignedUrl("getObject", { Bucket: bucket, Key: uploadFilename, Expires: 60000 })
+    const resizedImageBuff = sharp(uploadBuff).resize(270, 180);
+
+    const resizedParams ={
+        Bucket: bucket,
+        Key: "resized/" + uploadFilename,
+        Body: resizedImageBuff
+    }
+
+    s3.upload(resizedParams, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`Resized File ${uploadFilename} uploaded successfully to Bucket ${bucket}/resized.`);
+    });
+
+    const imgSignedS3Url = s3.getSignedUrl("getObject", { Bucket: bucket, Key: uploadFilename, Expires: 60000 })
+
+    const resizedImgsignedS3Url = s3.getSignedUrl("getObject", { Bucket: bucket, Key: "resized/" + uploadFilename, Expires: 60000 })
 
     const responseBody = {
         "bucket": bucket,
         "mimeType": uploadContetntType,
         "fileName": uploadFilename,
-        "signedUrl": signedS3Url,
+        "signedUrl": imgSignedS3Url,
+        "resizedImageSignedUrl": resizedImgsignedS3Url
     };
 
     callback(null,{
