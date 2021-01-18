73f7e420d099590af1c9ff827579365c27fa2d0c
xPlorinRolyPoly <alpanachaphalkar@gmail.com>
2021-01-18 14:48:05 +0100

    input validation functionality added.

--- application/app.js
+++ application/app.js
@@ -1,67 +1,103 @@
-const multipart = require("parse-multipart");
 const AWS = require("aws-sdk");
 const sharp = require('sharp');
+const parser = require('lambda-multipart-parser');
+const Joi = require('joi');
 const s3 = new AWS.S3();
 
-const bucket = process.env.BUCKET_NAME
+const BUCKET_NAME = process.env.BUCKET_NAME;
+const VALID_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/gif'];
 
-exports.lambdaHandler = function(event,context,callback){
-    console.log(event)
-    const bodyBuffer = new Buffer(event['body'].toString(),'base64');
-    const boundary = multipart.getBoundary(event.headers["Content-Type"]);
+let response = {
+    "isBase64Encoded": false,
+    "headers": {
+        "Content-Type": "application/json"
+    }
+}
 
-    const parts = multipart.Parse(bodyBuffer, boundary);
-    console.log(parts)
-    const uploadFilename = parts[0].filename;
-    const uploadBuff = parts[0].data;
-    const uploadContetntType = parts[0].type;
+exports.lambdaHandler = async function(event){
+    try {
+        console.log(event);
+        const parsedEvent = await parser.parse(event);
+        console.log('Result:', parsedEvent);
+        let resp = validateBody(parsedEvent);
+        if(isValidResponse(resp.statusCode)) {
+            const uploadContentType = parsedEvent.files[0].contentType;
+            const uploadBuff = parsedEvent.files[0].content;
+            const uploadFilename = parsedEvent.files[0].filename;
 
-    const params = {
-        Bucket: bucket,
-        Key: uploadFilename,
-        Body: uploadBuff
-    };
-
-    s3.upload(params, function(err, data) {
-        if (err) {
-            throw err;
+            const resizedImageBuff = sharp(uploadBuff).resize(200, 200, {
+                fit: sharp.fit.fill
+            });
+            const originalImage = await uploadImage("images/" + uploadFilename, uploadBuff);
+            const resizedImage = await uploadImage("images/resized/" + uploadFilename, resizedImageBuff);
+            let responseBody = {
+                "bucket": BUCKET_NAME,
+                "mimeType": uploadContentType,
+                "fileName": uploadFilename,
+                "imageUrl": originalImage.Location,
+                "resizedImageUrl": resizedImage.Location
+            }
+            response.body = JSON.stringify(responseBody);
+            response.statusCode = 201;
         }
-        console.log(`File ${uploadFilename} uploaded successfully to Bucket ${bucket}.`);
-    });
-
-    const resizedImageBuff = sharp(uploadBuff).resize(270, 180);
-
-    const resizedParams ={
-        Bucket: bucket,
-        Key: "resized/" + uploadFilename,
-        Body: resizedImageBuff
+    } catch (error) {
+        console.log(error);
     }
 
-    s3.upload(resizedParams, function(err, data) {
-        if (err) {
-            throw err;
-        }
-        console.log(`Resized File ${uploadFilename} uploaded successfully to Bucket ${bucket}/resized.`);
-    });
+    return response;
+}
 
-    const imgSignedS3Url = s3.getSignedUrl("getObject", { Bucket: bucket, Key: uploadFilename, Expires: 60000 })
-
-    const resizedImgsignedS3Url = s3.getSignedUrl("getObject", { Bucket: bucket, Key: "resized/" + uploadFilename, Expires: 60000 })
-
-    const responseBody = {
-        "bucket": bucket,
-        "mimeType": uploadContetntType,
-        "fileName": uploadFilename,
-        "signedUrl": imgSignedS3Url,
-        "resizedImageSignedUrl": resizedImgsignedS3Url
+async function uploadImage(bucketKey, bufferData) {
+    let respData = null;
+    const params = {
+        ACL: "public-read",
+        Bucket: BUCKET_NAME,
+        Key: bucketKey,
+        Body: bufferData
     };
+    console.log(params);
+    try {
+        respData = await s3.upload(params).promise();
+        console.log(respData);
+    } catch (error) {
+        console.log(error);
+    }
+    return respData;
+}
 
-    callback(null,{
-        "isBase64Encoded": false,
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": JSON.stringify(responseBody)
+function isValidResponse(statusCode){
+    return parseInt(statusCode) == 200;
+}
+
+function validateBody(parsedEvent){
+    const bodySchema = Joi.object({
+        files: Joi.array().max(1).min(1).required()
     });
+    let { error } = bodySchema.validate(parsedEvent);
+
+    let responseBody = {};
+    if ( error ){
+        responseBody = {
+            "status": "Error: Bad request body",
+            "message": error.details[0].message
+        };
+        response.body = JSON.stringify(responseBody);
+        response.statusCode = 400;
+        return response;
+    }
+
+    const contentType = parsedEvent.files[0].contentType;
+    if ( !VALID_IMAGE_TYPES.includes(contentType) ){
+        const search = ',';
+        const replacer = new RegExp(search, 'g');
+        responseBody = {
+            "status": "Error: Bad request body",
+            "message": "\"file\" must be of type " + VALID_IMAGE_TYPES.toString().replace(replacer, ' or ')
+        };
+        response.body = JSON.stringify(responseBody);
+        response.statusCode = 400;
+        return response;
+    }
+    response.statusCode = 200;
+    return response;
 }
